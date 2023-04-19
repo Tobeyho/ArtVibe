fbb7f38430c5a6f68dccba7e29092b27f58b5019
Tobeyho <93186041+Tobeyho@users.noreply.github.com>
2023-04-20 05:09:19 +0800

    Add files via upload

---
+++ ArtVibe/website/auth.py
@@ -0,0 +1,87 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db   ##means from __init__.py import db
+from flask_login import login_user, login_required, logout_user, current_user
+
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.gallery'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+                password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('Account created!', category='success')
+            return redirect(url_for('auth.login'))
+
+    return render_template("sign_up.html", user=current_user)
+
+
+@auth.route('/gallery', methods=['GET', 'POST'])
+def gallery():
+    return render_template('gallery.html', user=current_user)
+
+@auth.route('/merchStore', methods=['GET', 'POST'])
+def merchStore():
+    return render_template('merchStore.html', user=current_user)
+
+@auth.route('/events', methods=['GET', 'POST'])
+def events():
+    return render_template('events.html', user=current_user)
+
+@auth.route('/contact', methods=['GET', 'POST'])
+def contact():
+    return render_template('contact.html', user=current_user)
+
+@auth.route('/base')
+def base():
+    return render_template('base.html')
---
+++ ArtVibe/website/views.py
@@ -0,0 +1,54 @@
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def gallery():
+    return render_template('gallery.html', user=current_user)
+
+@views.route('/merchStore', methods=['GET', 'POST'])
+@login_required
+def merchStore():
+    return render_template('merchStore.html', user=current_user)
+
+@views.route('/events', methods=['GET', 'POST'])
+@login_required
+def events():
+    return render_template('events.html', user=current_user)
+
+             
+@views.route('/contact', methods=['GET', 'POST'])
+@login_required
+def contact():
+    if request.method == 'POST': 
+        note = request.form.get('note')#Gets the note from HTML 
+
+        if len(note) < 2:
+            flash('Note is too short!', category='error') 
+        else:
+            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
+            db.session.add(new_note) #adding the note to the database 
+            db.session.commit()
+            flash('Note added!', category='success')
+
+    return render_template("contact.html", user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():  
+    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
+
